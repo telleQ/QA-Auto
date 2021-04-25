701b2299d71aba029a28e2d4e13cb4c808af3ee5
Anastasiia <70407098+telleQ@users.noreply.github.com>
2021-04-25 22:00:46 +0300

    PupilDiaries

---
+++ WorkWithPupilDiaries/MainClass.cs
@@ -0,0 +1,186 @@
+﻿using System;
+
+namespace WorkWithPupilDiaries
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            string pupilName = Menu.AskPupilName();
+            // Создаем экземпляр класса PupilDiary.
+            PupilDiary diary = new PupilDiary();
+            diary.PupilName = pupilName;
+
+            while (true)
+            {
+                //Вывод меню с пунктами: добавить оценку, показать статистику, исправить оценку или выйти из программы.
+                string menuPoint = Menu.AskMainMenuPoint();
+
+                Console.Clear();
+                switch (menuPoint)
+                {
+                    // Выбор предмета из списка меню.
+                    case "1":
+                        string menuPointWithSubject = Menu.AskSubjectPoint();
+
+                        Console.Clear();
+                        switch (menuPointWithSubject)
+                        {
+                            case "1":
+                                double pupilMark = Menu.ReadMark();
+                                diary.AddMarks(pupilMark, diary.MarksInMathematics);
+                                diary.AddMarks(pupilMark, diary.PupilMarks);
+                                break;
+                            case "2":
+                                pupilMark = Menu.ReadMark();
+                                diary.AddMarks(pupilMark, diary.MarksInReading);
+                                diary.AddMarks(pupilMark, diary.PupilMarks);
+                                break;
+                            case "3":
+                                pupilMark = Menu.ReadMark();
+                                diary.AddMarks(pupilMark, diary.MarksInNaturalHistory);
+                                diary.AddMarks(pupilMark, diary.PupilMarks);
+                                break;
+                            case "4":
+                                pupilMark = Menu.ReadMark();
+                                diary.AddMarks(pupilMark, diary.MarksInGym);
+                                diary.AddMarks(pupilMark, diary.PupilMarks);
+                                break;
+                            case "5":
+                                pupilMark = Menu.ReadMark();
+                                diary.AddMarks(pupilMark, diary.MarksInWriting);
+                                diary.AddMarks(pupilMark, diary.PupilMarks);
+                                break;
+                            case "6":
+                                break;
+                            default:
+                                Menu.ShowInvalidMenuPointText();
+                                break;
+                        }
+                        break;
+
+                    // Вывод меню с пунктами: вывод статистики, возврат в предыдущее меню, выход с программы.
+                    case "2":
+                        string menuPointSubjectWithStatistic = Menu.AskStatisticPoint();
+                        Console.Clear();
+                        switch (menuPointSubjectWithStatistic)
+                        {
+                            case "1":
+                                diary.ShowStatistics(diary.MarksInMathematics);
+                                break;
+                            case "2":
+                                diary.ShowStatistics(diary.MarksInReading);
+                                break;
+                            case "3":
+                                diary.ShowStatistics(diary.MarksInNaturalHistory);
+                                break;
+                            case "4":
+                                diary.ShowStatistics(diary.MarksInGym);
+                                break;
+                            case "5":
+                                diary.ShowStatistics(diary.MarksInWriting);
+                                break;
+                            case "6":
+                                diary.ShowStatistics(diary.PupilMarks);
+                                break;
+                            // Возвращает в предыдущее меню.
+                            case "7":
+                                break;
+                            // Выйти из программы.
+                            case "8":
+                                return;
+                            default:
+                                Menu.ShowInvalidMenuPointText();
+                                break;
+                        }
+                        break;
+
+                    // Исправление ранее поставленных оценок по предметам.
+                    case "3":
+                        string _menuPointWithSubject = Menu.AskSubjectPoint();
+                        // Номер оценки по списку.
+                        int numberMark;
+                        // Новая оценка взамен старой.
+                        double newMark;
+                        Console.Clear();
+                        switch (_menuPointWithSubject)
+                        {
+                            case "1":
+                                if (Menu.ShowMarkOfSubject(diary.MarksInMathematics))
+                                {
+                                    Menu.ShowMarkOfSubject(diary.MarksInMathematics);
+                                    numberMark = Menu.AskNumberMark(diary.MarksInMathematics);
+                                    newMark = Menu.ReadMark();
+                                    diary.CorrectMarkOnSubject(diary.MarksInMathematics, numberMark, newMark);
+                                    Console.Clear();
+                                    Menu.ShowTitleOfCorrectMarks();
+                                    Menu.ShowMarkOfSubject(diary.MarksInMathematics);
+                                }
+                                break;
+                            case "2":
+                                if (Menu.ShowMarkOfSubject(diary.MarksInReading))
+                                {
+                                    Menu.ShowMarkOfSubject(diary.MarksInReading);
+                                    numberMark = Menu.AskNumberMark(diary.MarksInReading);
+                                    newMark = Menu.ReadMark();
+                                    diary.CorrectMarkOnSubject(diary.MarksInReading, numberMark, newMark);
+                                    Console.Clear();
+                                    Menu.ShowTitleOfCorrectMarks();
+                                    Menu.ShowMarkOfSubject(diary.MarksInReading);
+                                }
+                                break;
+                            case "3":
+                                if (Menu.ShowMarkOfSubject(diary.MarksInNaturalHistory))
+                                {
+                                    Menu.ShowMarkOfSubject(diary.MarksInNaturalHistory);
+                                    numberMark = Menu.AskNumberMark(diary.MarksInNaturalHistory);
+                                    newMark = Menu.ReadMark();
+                                    diary.CorrectMarkOnSubject(diary.MarksInNaturalHistory, numberMark, newMark);
+                                    Console.Clear();
+                                    Menu.ShowTitleOfCorrectMarks();
+                                    Menu.ShowMarkOfSubject(diary.MarksInNaturalHistory);
+                                }
+                                break;
+                            case "4":
+                                if (Menu.ShowMarkOfSubject(diary.MarksInGym))
+                                {
+                                    Menu.ShowMarkOfSubject(diary.MarksInGym);
+                                    numberMark = Menu.AskNumberMark(diary.MarksInGym);
+                                    newMark = Menu.ReadMark();
+                                    diary.CorrectMarkOnSubject(diary.MarksInGym, numberMark, newMark);
+                                    Console.Clear();
+                                    Menu.ShowTitleOfCorrectMarks();
+                                    Menu.ShowMarkOfSubject(diary.MarksInGym);
+                                }
+                                break;
+                            case "5":
+                                if (Menu.ShowMarkOfSubject(diary.MarksInWriting))
+                                {
+                                    Menu.ShowMarkOfSubject(diary.MarksInWriting);
+                                    numberMark = Menu.AskNumberMark(diary.MarksInWriting);
+                                    newMark = Menu.ReadMark();
+                                    diary.CorrectMarkOnSubject(diary.MarksInWriting, numberMark, newMark);
+                                    Console.Clear();
+                                    Menu.ShowTitleOfCorrectMarks();
+                                    Menu.ShowMarkOfSubject(diary.MarksInWriting);
+                                }
+                                break;
+                            case "6":
+                                break;
+                            default:
+                                Menu.ShowInvalidMenuPointText();
+                                break;
+                        }
+                        break;
+                    // Выйти из программы.
+                    case "4":
+                        return;
+                    // Если пользователь совершает неправильный выбор, то выводит ошибку.
+                    default:
+                        Menu.ShowInvalidMenuPointText();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ WorkWithPupilDiaries/Menu.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkWithPupilDiaries
+{
+    static class Menu
+    {
+        private static PupilDiary _diary = new PupilDiary();
+
+        /// <summary>
+        /// Основное меню. Спрашивает пользователя какую опцию меню хочет выбрать.
+        /// </summary>
+        /// <returns> Возвращает пункт меню, который выбрал пользователь. </returns>
+        public static string AskMainMenuPoint()
+        {
+            Console.WriteLine($@"Please select an option (what do you want to do):
+1 - if you want to add a score to diary;
+2 - if you want program to show statistic (max, min, average score);
+3 - if you want to correct a score;
+4 - if you want to exit this program.");
+
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Спрашивает имя ученика.
+        /// </summary>
+        /// <returns> Возвращает строку, которую ввел пользователь. </returns>
+        public static string AskPupilName()
+        {
+            Console.WriteLine("Please, enter a pupil's name: ");
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Выводит ошибку при выборе несуществующего пункта меню.
+        /// </summary>
+        public static void ShowInvalidMenuPointText()
+        {
+            Console.WriteLine("Please provide a valid data!\n");
+        }
+
+        /// <summary>
+        /// Выбор предмета из меню с предметами.
+        /// </summary>
+        /// <returns> Возвращаем строку c выбором пользователя. </returns>
+        public static string AskSubjectPoint()
+        {
+            Console.WriteLine($@"Please, select a subject from list:
+1 - mathematics;
+2 - reading; 
+3 - natural history;
+4 - gym;
+5 - writing;
+6 - go to the previous menu.");
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Меню со статистикой. Спрашивает пользователя какую опцию хочет выбрать.
+        /// </summary>
+        /// <returns> Возвращаем строку c выбором пользователя. </returns>
+        public static string AskStatisticPoint()
+        {
+            Console.WriteLine($@"Please, select an option:
+1 - show statistics in mathematics;
+2 - show statistics in reading; 
+3 - show statistics in natural history;
+4 - show statistics in gym;
+5 - show statistics in writing;
+6 - show general statistic;
+7 - go to the previous menu;
+8 - exit this program.");
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Показывает оценки по конкретному предмету.
+        /// </summary>
+        /// <param name="subjects"> Список предметов с оценками. </param>
+        public static bool ShowMarkOfSubject(List<double> subjects)
+        {
+            bool ifDiaryIsEmpty = false;
+            if (subjects.Count >= 1)
+            {
+                // Нумерация списка оценок.
+                int countElement = 1;
+                foreach (double markElement in subjects)
+                {
+                    Console.WriteLine("Mark " + countElement + ": " + markElement);
+                    countElement++;
+                }
+                ifDiaryIsEmpty = true;
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Your diary is empty.\n");
+                ifDiaryIsEmpty = false;
+            }
+            return ifDiaryIsEmpty;
+        }
+
+        /// <summary>
+        /// Спрашивает номер оценки, которую пользователь хочет исправить.
+        /// </summary>
+        /// <param name="subjects"> Список предметов с оценками. </param>
+        /// <returns> Номер оценки. </returns>
+        public static int AskNumberMark(List<double> subjects)
+        {
+            int numberMark;
+            bool isInputCorrect = false;
+            do
+            {
+                Console.WriteLine("Enter the mark number you want to change: ");
+                isInputCorrect = int.TryParse(Console.ReadLine(), out numberMark);
+                if (isInputCorrect == true && numberMark <= subjects.Count)
+                {
+                    break;
+                }
+                else
+                {
+                    ShowInvalidMenuPointText();
+                }
+            } while (true);
+            return numberMark;
+        }
+
+        /// <summary>
+        /// Спрашивает оценку от 1 до 10.
+        /// </summary>
+        /// <returns> Оценка. </returns>
+        public static double ReadMark()
+        {
+            double mark;
+            bool isInputCorrect = false;
+            do
+            {
+                Console.WriteLine("Please, enter a mark range from 1 to 10: ");
+                isInputCorrect = double.TryParse(Console.ReadLine(), out mark);
+                if (isInputCorrect == true && mark >= 1 && mark <= 10)
+                {
+                    break;
+                }
+                else
+                {
+                    ShowInvalidMenuPointText();
+                }
+            } while (true);
+            return mark;
+        }
+        /// <summary>
+        /// Показывает уведомление перед списком с исправленными оценками.
+        /// </summary>
+        public static void ShowTitleOfCorrectMarks()
+        {
+            Console.WriteLine("Diary with corrected marks:\n");
+        }
+    }
+}
